145061366acc319ba5fb66e147fe435d9bf721da
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-03-18 19:44:27 -0400

    optimize for single address case

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 var faucets = require('tbtc-faucets')
 var bitcoin = require('bitcoinjs-lib')
+var dezalgo = require('dezalgo')
+var noop = function() {}
 
 function Charger(wallet) {
   this._wallet = wallet
@@ -27,17 +29,19 @@ Charger.prototype.charge = function(address, amount) {
 
 Charger.prototype.execute = function(callback) {
   var self = this
+  var single = this._to.length === 1
+  if (single) this._from = this._to[0][0]
 
   this._faucet.withdraw(this._from, this._total, function(err, data) {
     if (err) return callback(err)
 
     self._txId = data.id
-    self._waitFor(self._txId, self._distribute.bind(self, callback))
+    var done = single ? callback : self._distribute.bind(self, callback)
+    self._waitFor(self._txId, done)
   })
 }
 
 Charger.prototype._distribute = function(callback) {
-  var self = this
   var distTx = this._builder.build()
   this._wallet.sendTx(distTx, callback)
 }
